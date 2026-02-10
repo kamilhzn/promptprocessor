e454f3f0215351830305d80d1c376b497db8c5d8
kamilhzn <3436055238@qq.com>
2026-02-10 22:54:04 +0800

    添加提示词库记录

---
+++ src/promptprocessor/prompts.py
@@ -0,0 +1,19 @@
+# 记录提示词库
+
+styles = {
+    "锐利硬朗": ["blade (galaxist)", "chainsaw man", "black rock shooter", "helltaker", "project moon"],
+    "圆润柔和": ["kemono friends", "yuru yuri", "furball", "daigaijin", "aogami", "naga u"],
+    "细腻精致": [
+        "fate/grand order",
+        "arknights",
+        "honkai (series)",
+        "granblue fantasy",
+        "princess connect!",
+        "dairi",
+        "kou hiyoyo",
+        "erobos",
+    ],
+    "简约干净": ["girls und panzer", "touken ranbu", "neptune (series)", "danganronpa (series)", "tani takeshi", "hammer (sunset beach)"],
+    "传统和风": ["ruu (tksymkw)", "itomugi-kun"],
+    "青春日常": ["girls band cry", "persona", "kouji (campus life)"],
+}
