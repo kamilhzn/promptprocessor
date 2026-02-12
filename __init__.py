5ca65526103636462d11ef1386a60e048c0c3542
kamilhzn <3436055238@qq.com>
2026-02-12 11:41:44 +0800

    重构节点导入路径，添加图像元素提取和颜色处理功能

--- __init__.py
+++ __init__.py
@@ -9,5 +9,5 @@ __author__ = """Worldjks"""
 __email__ = "3436055238@qq.com"
 __version__ = "0.0.1"
 
-from .src.promptprocessor.nodes import NODE_CLASS_MAPPINGS
-from .src.promptprocessor.nodes import NODE_DISPLAY_NAME_MAPPINGS
+from .src.nodes import NODE_CLASS_MAPPINGS
+from .src.nodes import NODE_DISPLAY_NAME_MAPPINGS
