7cb30d3fe742f7a50211cc323d4f0bfb3910c2bd
kamilhzn <3436055238@qq.com>
2026-02-13 15:14:12 +0800

    更新PromptCombine类以支持样式随机选择，移除ElementTransform类

--- src/promptprocessor/prompt_use.py
+++ src/promptprocessor/prompt_use.py
@@ -1,4 +1,6 @@
 from inspect import cleandoc
+from .prompts import styles
+import random
 
 
 class PromptCombine:
@@ -29,7 +31,10 @@ class PromptCombine:
     def combine(self, style, sex, clothes, hair, face, weapon, obj, action, others):
         prompt = "very awa, best quality, masterpiece, highres, absurdres, "
         if style != "":
-            prompt += cleandoc(style) + ", "
+            if style in styles:
+                prompt += cleandoc(random.choice(styles[style])) + ", "
+            else:
+                prompt += "rurudo, "
         if sex != "":
             prompt += cleandoc(sex) + ", solo, full body, "
         if clothes != "":
@@ -80,48 +85,3 @@ class PromptEdit:
             return (
                 f"将图1中的{obj_str}与图2中的{obj_str}融合，保持图1的轮廓形状与纹理细节,保持图2的整体构图与风格，通过改变其方向与透视来使图1的{obj_str}完美地融入图2",
             )
-
-
-class ElementTransform:
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "element_str": ("STRING", {"default": "", "description": "输入要转换的元素描述"}),
-                "color_str": ("STRING", {"default": "", "description": "输入要转换的颜色描述"}),
-                "model": ("MODEL", {"default": "cocoballking", "description": "选择使用的转换模型"}),
-            },
-        }
-
-    RETURN_TYPES = ("STRING", "STRING", "STRING", "STRING", "STRING", "STRING", "STRING", "STRING", "STRING", "STRING")
-    RETURN_NAMES = (
-        "颜色",
-        "画风",
-        "性别",
-        "衣物",
-        "头发",
-        "面部",
-        "武器",
-        "物体",
-        "动作",
-        "其他",
-    )
-    FUNCTION = "transform_element"
-    CATEGORY = "Prompt Processor/element2prompt"
-    DESCRIPTION = "Transform an element description."
-
-    def transform_element(self, element_str, color_str):
-        # TODO:完善元素转换逻辑，目前先返回占位字符串
-        style = ""
-        sex = ""
-        clothes = ""
-        hair = ""
-        face = ""
-        weapon = ""
-        object = ""
-        action = ""
-        others = ""
-        return (color_str, style, sex, clothes, hair, face, weapon, object, action, others)
