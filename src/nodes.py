423ccc80ff576b9bed7ce73257b151a9d62b3bb3
kamilhzn <3436055238@qq.com>
2026-02-13 15:13:36 +0800

    修复元素转提示词的显示名称，调整导入语句以优化代码结构

--- src/nodes.py
+++ src/nodes.py
@@ -1,5 +1,6 @@
-from .promptprocessor.prompt_use import PromptCombine, PromptEdit, ElementTransform
+from .promptprocessor.prompt_use import PromptCombine, PromptEdit
 from .img2element.img_trans_elem import ImageElementGet
+from .elem2prompt.elem_trans_prompt import ElementTransform
 
 NODE_CLASS_MAPPINGS = {
     "PromptCombine": PromptCombine,
@@ -12,5 +13,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "PromptCombine": "提示词合并",
     "PromptEdit": "提示词编辑",
     "ImageElementGet": "图像元素提取",
-    "ElementTransform": "元素转人物词",
+    "ElementTransform": "元素转提示词",
 }
