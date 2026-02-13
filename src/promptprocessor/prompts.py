e0d6eca6fe66542d8b2eb21dee1b0065e650d547
kamilhzn <3436055238@qq.com>
2026-02-13 15:13:51 +0800

    移除多余空行，优化代码格式

--- src/promptprocessor/prompts.py
+++ src/promptprocessor/prompts.py
@@ -1,5 +1,4 @@
 # 记录提示词库
-
 styles = {
     "锐利硬朗": ["blade (galaxist)", "chainsaw man", "black rock shooter", "helltaker", "project moon"],
     "圆润柔和": ["kemono friends", "yuru yuri", "furball", "daigaijin", "aogami", "naga u"],
@@ -17,3 +16,10 @@ styles = {
     "传统和风": ["ruu (tksymkw)", "itomugi-kun"],
     "青春日常": ["girls band cry", "persona", "kouji (campus life)"],
 }
+
+
+if __name__ == "__main__":
+    import random
+
+    prompt = random.choice(styles["细腻精致"])
+    print(prompt)
